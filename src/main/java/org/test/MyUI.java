c270abfcb78d9c91d301d0e127c67ba8dafc9686
Artem Godin <artem.godin@finago.com>
2022-03-14 10:23:49 +0200

    Issue demonstration

--- src/main/java/org/test/MyUI.java
+++ src/main/java/org/test/MyUI.java
@@ -1,42 +1,50 @@
 package org.test;
 
-import javax.servlet.annotation.WebServlet;
-
 import com.vaadin.annotations.Theme;
 import com.vaadin.annotations.VaadinServletConfiguration;
 import com.vaadin.server.VaadinRequest;
 import com.vaadin.server.VaadinServlet;
-import com.vaadin.ui.Button;
-import com.vaadin.ui.Label;
-import com.vaadin.ui.TextField;
-import com.vaadin.ui.UI;
-import com.vaadin.ui.VerticalLayout;
+import com.vaadin.ui.*;
+
+import javax.servlet.annotation.WebServlet;
 
 /**
- * This UI is the application entry point. A UI may either represent a browser window 
+ * This UI is the application entry point. A UI may either represent a browser window
  * (or tab) or some part of an HTML page where a Vaadin application is embedded.
  * <p>
- * The UI is initialized using {@link #init(VaadinRequest)}. This method is intended to be 
+ * The UI is initialized using {@link #init(VaadinRequest)}. This method is intended to be
  * overridden to add component to the user interface and initialize non-component functionality.
  */
 @Theme("mytheme")
 public class MyUI extends UI {
+    private VerticalLayout firstLayout = new VerticalLayout(new Label("Visible by default"));
+    private VerticalLayout secondLayout = new VerticalLayout(new Label("Hidden by default"));
 
     @Override
     protected void init(VaadinRequest vaadinRequest) {
         final VerticalLayout layout = new VerticalLayout();
-        
-        final TextField name = new TextField();
-        name.setCaption("Type your name here:");
 
-        Button button = new Button("Click Me");
-        button.addClickListener(e -> {
-            layout.addComponent(new Label("Thanks " + name.getValue() 
-                    + ", it works!"));
+        secondLayout.setVisible(false);
+        layout.addComponents(firstLayout, secondLayout);
+
+        Label text = new Label("Open two browser tabs, then click 'Switch content' on any");
+
+        Button switchLayouts = new Button("Switch content on other tabs", e -> {
+            for (UI ui : UI.getCurrent().getSession().getUIs()) {
+                if (ui != this) {
+                    // Switch content on other tabs
+                    MyUI myUi = (MyUI) ui;
+                    myUi.access(() -> {
+                        myUi.firstLayout.setVisible(false);
+                        myUi.secondLayout.setVisible(true);
+                    });
+                    Notification.show("Observe AssertionError in the server console");
+                }
+            }
         });
-        
-        layout.addComponents(name, button);
-        
+
+        layout.addComponents(text, switchLayouts);
+
         setContent(layout);
     }
 
